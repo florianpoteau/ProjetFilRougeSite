03634d97d0a2b62832df1064163e007aba4d7f8c
Poteau Florian <florianpoteau59@gmail.com>
2022-12-20 15:53:41 +0100

    push

--- src/main/java/Fil_Rouge/MethodRéservation.java
+++ src/main/java/Fil_Rouge/MethodRéservation.java
@@ -1,5 +1,17 @@
 package Fil_Rouge;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class MethodRéservation {
+	
+	private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("sandwich");
+	
+	public static void exporterDonnée() {
+	
+    
+  
+	}
 
 }

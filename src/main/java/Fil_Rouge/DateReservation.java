64e34b855dc13e32d9d5f1f1aac15b04ed7eda6b
mehdijouihri <100852016+mehdijouihri@users.noreply.github.com>
2022-12-22 16:28:02 +0100

    test spring

---
+++ src/main/java/Fil_Rouge/DateReservation.java
@@ -0,0 +1,17 @@
+package Fil_Rouge;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class DateReservation {
+	
+	private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("sandwich");
+	
+	
+}
--- src/main/java/Fil_Rouge/Main.java
+++ src/main/java/Fil_Rouge/Main.java
@@ -1,8 +1,13 @@
 package Fil_Rouge;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		
+		SpringApplication.run(Main.class,args);
 	}
 }
